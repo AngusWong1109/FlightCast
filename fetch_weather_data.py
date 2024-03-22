838a65691eeb46fec7c0702827f2cc94ae44da27
Angus Wong <58066213+AngusWong1109@users.noreply.github.com>
2024-03-22 11:40:57 -0700

    fetch weather data

--- fetch_weather_data.py
+++ fetch_weather_data.py
@@ -1,9 +1,15 @@
 from requests import get
-import pandas as pd
-import numpy as np
+from datetime import datetime, timedelta
 
-url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Hong%20Kong%20International%20Airport/2024-03-19/2024-03-20?unitGroup=metric&include=hours&key=ZDLURKHWM6GZRFDUSAL75YSWN&contentType=csv'
-response = get(url)
-csv_file = open('weather_data.csv', 'wb')
-csv_file.write(response.content)
+url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Hong%20Kong%20International%20Airport/{}/{}?unitGroup=metric&include=hours&key=3TX3Y9V92EQVZ7SG4ZT6PKEDW&contentType=csv'
+start_date = datetime(2023,12,23)
+end_date = datetime(2024,3,20)
+csv_file = open('weather_data.csv', 'ab')
+
+while start_date < end_date:
+    next_date = start_date + timedelta(days=1)
+    response = get(url.format(start_date.strftime('%Y-%m-%d'), next_date.strftime('%Y-%m-%d')))
+    csv_file.write(response.content)
+    start_date = next_date
+
 csv_file.close()
