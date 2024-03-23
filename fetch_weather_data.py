ebc42cd24054572cec5df63f9a241c7136b27595
Angus Wong <58066213+AngusWong1109@users.noreply.github.com>
2024-03-22 23:23:06 -0700

    Fetched weather data from 2023-12-23 to 2024-1-27

--- fetch_weather_data.py
+++ fetch_weather_data.py
@@ -2,14 +2,17 @@ from requests import get
 from datetime import datetime, timedelta
 
 url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Hong%20Kong%20International%20Airport/{}/{}?unitGroup=metric&include=hours&key=3TX3Y9V92EQVZ7SG4ZT6PKEDW&contentType=csv'
-start_date = datetime(2023,12,23)
+start_date = datetime(2024,1,28)
 end_date = datetime(2024,3,20)
 csv_file = open('weather_data.csv', 'ab')
 
 while start_date < end_date:
     next_date = start_date + timedelta(days=1)
     response = get(url.format(start_date.strftime('%Y-%m-%d'), next_date.strftime('%Y-%m-%d')))
-    csv_file.write(response.content)
-    start_date = next_date
+    if response.status_code == 200:
+        csv_file.write(response.content)
+        start_date = next_date
+    else:
+        break
 
 csv_file.close()
