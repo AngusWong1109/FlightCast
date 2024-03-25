af16da667dac4e36615b91b0992e050dc7912f86
Angus Wong <58066213+AngusWong1109@users.noreply.github.com>
2024-03-25 16:02:15 -0700

    Changed 'coded_status' to 'label'

--- process_flight_data.py
+++ process_flight_data.py
@@ -8,7 +8,7 @@ pass_dep = pd.read_csv('pass_departure_data.csv')
 selected_columns = ['date', 'arrival', 'time', 'flight', 'status', 'destination']
 pass_dep = pass_dep[selected_columns]
 pass_dep['actual time diff'] = None
-pass_dep['coded_status'] = None
+pass_dep['label'] = None
 for i in range(pass_dep.shape[0]):
     time_split = pass_dep['status'][i].split()
     try:
@@ -31,15 +31,15 @@ for i in range(pass_dep.shape[0]):
     time_diff_mins = time_diff.total_seconds() / 60
     pass_dep.loc[i, 'actual time diff'] = time_diff_mins
     if time_diff_mins > 120:
-        pass_dep.loc[i, 'coded_status'] = 'Delayed more than 2 hours'
+        pass_dep.loc[i, 'label'] = 'Delayed more than 2 hours'
     elif time_diff_mins > 60:
-        pass_dep.loc[i, 'coded_status'] = 'Delayed 1 to 2 hours'
+        pass_dep.loc[i, 'label'] = 'Delayed 1 to 2 hours'
     elif time_diff_mins > 30:
-        pass_dep.loc[i, 'coded_status'] = 'Delayed 30 mins to 1 hours'
+        pass_dep.loc[i, 'label'] = 'Delayed 30 mins to 1 hours'
     elif time_diff_mins > 5:
-        pass_dep.loc[i, 'coded_status'] = 'Delayed 5 to 30 mins'
+        pass_dep.loc[i, 'label'] = 'Delayed 5 to 30 mins'
     else:
-        pass_dep.loc[i, 'coded_status'] = 'On time'
+        pass_dep.loc[i, 'label'] = 'On time'
 
 df_file = pass_dep.to_csv('processed_pass_dep_data.csv')
 
@@ -49,7 +49,7 @@ pass_arr = pd.read_csv('pass_arrival_data.csv')
 selected_columns = ['date', 'arrival', 'time', 'flight', 'status', 'origin']
 pass_arr = pass_arr[selected_columns]
 pass_arr['actual time diff'] = None
-pass_arr['coded_status'] = None
+pass_arr['label'] = None
 for i in range(pass_arr.shape[0]):
     time_split = pass_arr['status'][i].split()
     try:
@@ -72,14 +72,14 @@ for i in range(pass_arr.shape[0]):
     time_diff_mins = time_diff.total_seconds() / 60
     pass_arr.loc[i, 'actual time diff'] = time_diff_mins
     if time_diff_mins > 120:
-        pass_arr.loc[i, 'coded_status'] = 'Delayed more than 2 hours'
+        pass_arr.loc[i, 'label'] = 'Delayed more than 2 hours'
     elif time_diff_mins > 60:
-        pass_arr.loc[i, 'coded_status'] = 'Delayed 1 to 2 hours'
+        pass_arr.loc[i, 'label'] = 'Delayed 1 to 2 hours'
     elif time_diff_mins > 30:
-        pass_arr.loc[i, 'coded_status'] = 'Delayed 30 mins to 1 hours'
+        pass_arr.loc[i, 'label'] = 'Delayed 30 mins to 1 hours'
     elif time_diff_mins > 5:
-        pass_arr.loc[i, 'coded_status'] = 'Delayed 5 to 30 mins'
+        pass_arr.loc[i, 'label'] = 'Delayed 5 to 30 mins'
     else:
-        pass_arr.loc[i, 'coded_status'] = 'On time'
+        pass_arr.loc[i, 'label'] = 'On time'
 
 df_file = pass_arr.to_csv('processed_pass_arr_data.csv')
