56692454156fbaed6f08821ec25ea4c5bc2fc054
Angus Wong <58066213+AngusWong1109@users.noreply.github.com>
2024-03-25 23:19:12 -0700

    modified code

--- merge_data.py
+++ merge_data.py
@@ -4,13 +4,9 @@ from process_weather_data import process_weather_data
 
 # Access processed passenger arrival data
 pass_arr = process_flight_data('pass_arrival_data.csv')
-print(pass_arr)
 
 # Access processed passenger departure data
 pass_dep = process_flight_data('pass_departure_data.csv', arrival=False)
-print(pass_dep)
 
 # Access processed weather data
 weather_data = process_weather_data('weather_data.csv')
-# Change the date in the datat to datetime object
-weather_data.loc[:, 'datetime'] = pd.to_datetime(weather_data['datetime'])
--- process_weather_data.py
+++ process_weather_data.py
@@ -6,4 +6,6 @@ def process_weather_data(filepath):
     weather_data = weather_data[weather_data['name'] != 'name']
     selected_columns = ['datetime', 'temp', 'feelslike', 'dew', 'humidity', 'precip', 'precipprob', 'preciptype', 'snow', 'snowdepth', 'windgust', 'windspeed', 'winddir', 'sealevelpressure', 'cloudcover', 'visibility', 'solarradiation', 'solarenergy', 'uvindex', 'severerisk', 'conditions', 'icon']
     weather_data = weather_data[selected_columns]
+    # Change the date in the datat to datetime object
+    weather_data.loc[:, 'datetime'] = pd.to_datetime(weather_data['datetime'])
     return weather_data
