db611ee72738afe49bae208a2435962ba2e3391e
Angus Wong <58066213+AngusWong1109@users.noreply.github.com>
2024-03-25 23:01:45 -0700

    changed from pass_departure_data_new to pass_departure_data and pass_arrival_data_new to pass_arrival_data

--- merge_data.py
+++ merge_data.py
@@ -3,11 +3,11 @@ from process_flight_data import process_flight_data
 from process_weather_data import process_weather_data
 
 # Access processed passenger arrival data
-pass_arr = process_flight_data('pass_arrival_data_new.csv')
+pass_arr = process_flight_data('pass_arrival_data.csv')
 print(pass_arr)
 
 # Access processed passenger departure data
-pass_dep = process_flight_data('pass_departure_data_new.csv', arrival=False)
+pass_dep = process_flight_data('pass_departure_data.csv', arrival=False)
 print(pass_dep)
 
 # Access processed weather data
